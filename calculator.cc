c9a48fa1c696054033a24b03e85f0217ac59d96d
Lucca Carvalho Magalhaes <103290591+LuccaC-M@users.noreply.github.com>
2022-09-25 15:39:07 -0400

    Update calculator.cc

--- calculator.cc
+++ calculator.cc
@@ -1,20 +1,24 @@
 #include <iostream>
 
 int main() {
-
+//	Used variables
 	long num1;
 	long num2;
 	char oper;
 	
+//	1st number
 	std::cout << "1st number > ";
 	std::cin >> num1;
 	
+//	2nd number
 	std::cout << "2nd number > ";
 	std::cin >> num2;
-
+	
+//	asking for operation
 	std::cout << "Choose operation\n" << "+\n" << "-\n" << "*\n" << "/\n" << "%\n" << "> ";
 	std::cin >> oper;
 	
+// matching oper to and operation	
 	switch(oper) {
 		case '+':
 			std::cout << num1 + num2 << std::endl;
