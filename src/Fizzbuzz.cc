8324c853a9a572051e66600a1fc88ebec99fa646
Lucca Magalhaes <git.yfm@slmail.me>
2022-10-07 11:50:10 -0400

    Update Fizzbuzz.cc

--- src/Fizzbuzz.cc
+++ src/Fizzbuzz.cc
@@ -2,25 +2,31 @@
 
 #define MAX 10000
 
+bool check_factor(int n, int i) {
+  if (n % i == 0) {
+      return true;
+  }
+  else {
+        return false;
+  }
 int main() {
     
     int iMax;
 
     do {
-
         std::cout << "What is the maximum number > ";
         std::cin >> iMax;
     } while (iMax > MAX || iMax <= 0);
     
     for (int i = 1; i <= iMax; i++) {
         
-        if (i % 5 == 0 && i % 3 == 0) {
+        if (check_factor(i,5) && check_factor(i,3)) {
             std::cout << "FizzBuzz" << std::endl;
         }
-        else if (i % 3 == 0) {
+        else if (check_factor(i,3)) {
             std::cout << "Fizz" << std::endl;
         }
-        else if (i % 5 == 0) {
+        else if (check_factor(i,5)) {
             std::cout << "Buzz" << std::endl;
         }
         else {
